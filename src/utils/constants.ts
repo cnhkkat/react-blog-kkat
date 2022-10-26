4ccc8dc3852b48bb03305f769d74ff58ec173153
cnhkkat <1071541105@qq.com>
2022-10-26 10:59:09 +0800

    first commit

---
+++ src/utils/constants.ts
@@ -0,0 +1,17 @@
+export const env = 'kkat-blog-react-0gxmmir16fbc41e7'
+export const siteTitle = 'Ra薯小站';
+export const pageDesc = '人世间悲喜烂剧，昼夜轮播不停'
+// 透明头像
+export const welcomeCardAvatar = 'https://cdn.staticaly.com/gh/cnhkkat/images-store@master/transparent-avatar.webp'
+
+export const githubUrl = 'https://github.com/cnhkkat'
+
+export const csdnUrl = 'https://blog.csdn.net/Rashu99'
+
+export const loadingUrl = 'https://cdn.staticaly.com/gh/cnhkkat/images-store@master/loading.jpeg'
+
+// 首页文章分页，每页数据
+export const homeSize = 8;
+
+// 数据缓存时间
+export const staleTime = 180000;
